5bfdd6c7b6c95d771e41ccef0573c5bfa61f5db6
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-07 09:24:06 -0500

    DAY 1

---
+++ Week_1/Day_1/Stretch/reverse+.js
@@ -0,0 +1,14 @@
+const [node, script, ...args] = process.argv;
+
+function reverse(string) {
+  let finalArray = [];
+  let strArray = string.split('');
+
+  for (let i = strArray.length - 1; i >= 0; i-- ) {
+    finalArray.push(strArray[i]);
+  }
+  return finalArray.join('');
+};
+
+const reversedWords = args.map(arg => reverse(arg));
+console.log(reversedWords.join('\n'));
--- Week_1/Day_1/Stretch/reverse.js
+++ Week_1/Day_1/Stretch/reverse.js
@@ -7,8 +7,6 @@ function reverse(string) {
   for (let i = strArray.length - 1; i >= 0; i-- ) {
     finalArray.push(strArray[i]);
   }
-
-  console.assert(finalArray.length === strArray.length, 'The length of the input string and the reversed string should be equal');
   console.log(finalArray.join(''));
 };
 
