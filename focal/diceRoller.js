1f79b0bfe56739607c111b1ad73ac89e8841ff4b
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-07 14:15:35 -0500

    Create diceRoller.js

---
+++ focal/diceRoller.js
@@ -0,0 +1,18 @@
+const [node, script, number] = process.argv; 
+
+function getRandomDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function diceRoller(number) {
+    let resultArray = [];
+    let x = 1;
+    
+    while (x <= number) {
+        resultArray.push(getRandomDice());
+        x++
+    }; 
+    return resultArray.join(' ');
+};
+
+console.log(diceRoller(number));
