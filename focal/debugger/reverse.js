ab20200b5164ecf41d929e5b6cf047c1c67b6809
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-07 17:36:11 -0500

    Day 2

--- focal/debugger/reverse.js
+++ focal/debugger/reverse.js
@@ -1,8 +1,12 @@
-var input = process.argv[2];
+var input = process.argv.slice(2);
 
-if (input)
-  console.log(reverse(input));
+function reverse(input) {
 
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
+if (input !== null || input.length > 0) {
+  return input.toString().split('').reverse().join('');
 }
+return 'Invalid input';
+
+};
+
+console.log(reverse(input))
