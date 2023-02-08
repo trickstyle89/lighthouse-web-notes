32d0f58bb82a5147642f1eae4d1408ee4aa4c9ba
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-08 11:07:43 -0500

    Create raisin.js

---
+++ focal/objects/raisin.js
@@ -0,0 +1,11 @@
+const raisinAlarm = function (cookie) {
+  // Put your solution here
+};
+
+console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
+console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
+
+Raisin alert!
+Raisin alert!
+All good!
