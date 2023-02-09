9e79e46aaa9e3994b54a36e3b079645981c29cf0
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-09 13:24:03 -0500

    Update findWaldo.js

--- focal/functions/callbacks-intro && cd callbacks-intro/findWaldo.js
+++ focal/functions/callbacks-intro && cd callbacks-intro/findWaldo.js
@@ -1,14 +1,16 @@
-const findWaldo = function(names, found) {
-  for (let i = 0; i < names.length; i++) {
-    let name = names[i];
-    if (name === "Waldo") {
-      found();   // execute callback
+const findWaldo = function(names, actionWhenFound) {
+  names.forEach(name => {
+    if (name === 'Waldo') {
+      //pass the argument here as it is the only way for second function to get parameters.
+      actionWhenFound(names); 
     }
-  }
-}
+  });
+  return false;
+};
 
-const actionWhenFound = function() {
-  console.log("Found him!");
-}
+const actionWhenFound = function(names) {
+  let results = names.findIndex(element => element === 'Waldo')
+  console.log(results);
+};
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
