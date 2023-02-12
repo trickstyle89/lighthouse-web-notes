23f9386c826dd1e69e656c0ee142cfb2e827e5d0
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-12 12:04:43 -0500

    sunday

--- Week_1/Day_1/Stretch/sorting callback for objects.js
+++ Week_1/Day_1/Stretch/sorting callback for objects.js
@@ -4,3 +4,33 @@ const students = [
   { id: 3, name: "alex",     age: 22 },
   { id: 4, name: "alex",     age: 30 }
 ];
+
+// create a function to access the object.
+const printData = function(student) {
+  console.log(`Name: ${student.name} Age: ${student.age}`);
+};
+
+// callback to use age sort.
+
+const sortAge = function(students) {
+  return students.sort((a , b)=> a.age - b.age);
+};
+
+const sortName = function(students) {
+  return students.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+};
+
+// link it all together
+
+const sortedData = sortName(students);
+// const sortedData = sortAge(students);
+
+sortedData.forEach(student => printData(student));
