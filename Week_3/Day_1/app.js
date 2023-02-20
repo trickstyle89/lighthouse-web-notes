6d8524bdaab02adf9c4a59a5c221c5a496b28896
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-20 13:38:09 -0500

    Update app.js

--- Week_3/Day_1/app.js
+++ Week_3/Day_1/app.js
@@ -3,12 +3,21 @@ const PORT = 8080;
 
 // a function which handles requests and sends response
 const requestHandler = function (request, response) {
-  const responseText = `Requested Path: ${request.url}\nRequest Method: ${request.method}`;
-  response.end(responseText);
+  if (request.url === "/") {
+    response.end("Welcome!");
+  } else if (request.url === "/urls") {
+    response.end("www.lighthouselabs.ca\nwww.google.com");
+  } else {
+    response.statusCode = 404;
+    response.end("404 Page Not Found");
+  }
 };
 
 const server = http.createServer(requestHandler);
+console.log("Server created"); // NEW LINE
 
 server.listen(PORT, () => {
   console.log(`Server listening on: http://localhost:${PORT}`);
 });
+
+console.log("Last line (after .listen call)"); // NEW LINE
